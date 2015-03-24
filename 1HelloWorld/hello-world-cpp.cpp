198a8c30fc5037d3210d134afc74327bf40ddf53
Kawthar Alashoor <kawthar.ashour@kaust.edu.sa>
2015-03-24 18:19:34 +0000

    Homework 1 commit

---
+++ 1HelloWorld/hello-world-cpp.cpp
@@ -0,0 +1,26 @@
+/*
+* Hellow World Story. Modified from http://www.cplusplus.com/
+* g++ hello-world-cpp.cpp -o hellocpp.out
+* ./hellocpp.out
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+  string mystr;
+  cout << "What's your name? ";
+  getline (cin, mystr);
+  cout << "Hello \
+  \
+  \
+  \
+  \
+  asdasdasd" << mystr << ".\n";
+  cout << "What is your favorite book? ";
+  getline (cin, mystr);
+  cout << "I like " << mystr << " too!\n";
+  return 0;
+}
